cb58e3f180f651baf968b1385c5d81d252cbe6e6
Tech-goodat <felixkiprotich2000@gmail.com>
2023-10-05 15:32:39 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(books => renderBooks(books))
 }
 
-function renderBooks(books) {
+function renderBooks(book) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  book.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  return main;
+  
 }
+renderBooks();
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
+  
 });
